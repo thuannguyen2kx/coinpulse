b1074b1475ecb4b1581b972dc2bf359c3060fd1f
nguyenthanhthuan <thuan.nguyenthanh.contact@gmail.com>
2026-06-14 23:33:17 +0700

    implement coin overview and trending coins ui and functionality using the fetcher utility function

---
+++ components/home/trending-coins.tsx
@@ -0,0 +1,86 @@
+import DataTable from "@/components/data-table";
+import { TrendingCoinsFallback } from "@/components/home/fallback";
+import { fetcher } from "@/lib/coingecko.actions";
+import { cn, formatCurrency } from "@/lib/utils";
+import { TrendingDown, TrendingUp } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+const columns: DataTableColumn<TrendingCoin>[] = [
+  {
+    header: "Name",
+    cellClassName: "name-cell",
+    cell: (coin) => {
+      const item = coin.item;
+
+      return (
+        <Link href={`/coins/${item.id}`}>
+          <Image src={item.large} alt={item.name} width={36} height={36} />
+          <p>{item.name}</p>
+        </Link>
+      );
+    },
+  },
+  {
+    header: "24h Change",
+    cellClassName: "name-cell",
+    cell: (coin) => {
+      const item = coin.item;
+      const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
+
+      return (
+        <div
+          className={cn(
+            "price-change",
+            isTrendingUp ? "text-green-500" : "text-red-500",
+          )}
+        >
+          <p>
+            {isTrendingUp ? (
+              <TrendingUp width={16} height={16} />
+            ) : (
+              <TrendingDown width={16} height={16} />
+            )}
+            {Math.abs(item.data.price_change_percentage_24h.usd).toFixed(2)}%
+          </p>
+        </div>
+      );
+    },
+  },
+  {
+    header: "Price",
+    cellClassName: "price-cell",
+    cell: (coin) => formatCurrency(coin.item.data.price),
+  },
+];
+
+const TrendingCoins = async () => {
+  let trendingCoins;
+
+  try {
+    trendingCoins = await fetcher<{ coins: TrendingCoin[] }>(
+      "/search/trending",
+      undefined,
+      300,
+    );
+  } catch (error) {
+    console.error("Error fetching trending coins:", error);
+    return <TrendingCoinsFallback />;
+  }
+
+  return (
+    <div className="trending-coins">
+      <h4>Trending Coins</h4>
+
+      <DataTable
+        data={trendingCoins.coins.slice(0, 6) || []}
+        columns={columns}
+        rowKey={(coin) => coin.item.id}
+        headerCellClassName="py-3!"
+        bodyCellClassName="py-2!"
+      />
+    </div>
+  );
+};
+
+export default TrendingCoins;
---
+++ type.d.ts
@@ -0,0 +1,90 @@
+interface CoinDetailsData {
+  id: string;
+  name: string;
+  symbol: string;
+  asset_platform_id?: string | null;
+  detail_platforms?: Record<
+    string,
+    {
+      geckoterminal_url: string;
+      contract_address: string;
+    }
+  >;
+  image: {
+    large: string;
+    small: string;
+  };
+  market_data: {
+    current_price: {
+      usd: number;
+      [key: string]: number;
+    };
+    price_change_24h_in_currency: {
+      usd: number;
+    };
+    price_change_percentage_24h_in_currency: {
+      usd: number;
+    };
+    price_change_percentage_30d_in_currency: {
+      usd: number;
+    };
+    market_cap: {
+      usd: number;
+    };
+    total_volume: {
+      usd: number;
+    };
+  };
+  market_cap_rank: number;
+  description: {
+    en: string;
+  };
+  links: {
+    homepage: string[];
+    blockchain_site: string[];
+    subreddit_url: string;
+  };
+  tickers: Ticker[];
+}
+
+interface TrendingCoin {
+  item: {
+    id: string;
+    name: string;
+    symbol: string;
+    market_cap_rank: number;
+    thumb: string;
+    large: string;
+    data: {
+      price: number;
+      price_change_percentage_24h: {
+        usd: number;
+      };
+    };
+  };
+}
+
+interface DataTableColumn<T> {
+  header: React.ReactNode;
+  cell: (row: T, index: number) => React.ReactNode;
+  headClassName?: string;
+  cellClassName?: string;
+}
+
+interface DataTableProps<T> {
+  columns: DataTableColumn<T>[];
+  data: T[];
+  rowKey: (row: T, index: number) => React.Key;
+  tableClassName?: string;
+  headerClassName?: string;
+  headerRowClassName?: string;
+  headerCellClassName?: string;
+  bodyRowClassName?: string;
+  bodyCellClassName?: string;
+}
+
+interface CoinGeckoErrorBody {
+  error?: string;
+}
+
+type QueryParams = Record<string, string | number | boolean | undefined>;
